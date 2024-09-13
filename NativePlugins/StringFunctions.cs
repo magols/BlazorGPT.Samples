4098c8a57cc173720b378ad2c2ed3691f2db85eb
Magnus Olsson <magnus72@gmail.com>
2024-09-13 09:51:22 +0200

    Some sample plugins

--- NativePlugins/StringFunctions.cs
+++ NativePlugins/StringFunctions.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel;
 using Microsoft.SemanticKernel;
 
-namespace BlazorGPT.Samples.Native;
+namespace BlazorGPT.Samples;
 
-public class StringFunctions
+// note that all classes with plugin functions must be public and have a constructor that takes an IServiceProvider as a parameter
+public class StringFunctions(IServiceProvider serviceProvider)
 {
 
     [KernelFunction("count_chars")]
@@ -12,6 +13,24 @@ public class StringFunctions
     public int CountChars([Description("the text to count characters on")]string input)
     {
         return input.Length;
+    }
+
+    [KernelFunction("reverse_string")]
+    [Description("Reverses a string")]
+    [return: Description("The reversed string")]
+    public string ReverseString([Description("the text to reverse")] string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    [KernelFunction("concat_strings")]
+    [Description("Concatenates two strings")]
+    [return: Description("The concatenated string")]
+    public string ConcatStrings([Description("the first string")] string input1, [Description("the second string")] string input2)
+    {
+        return input1 + input2;
     }
 
 }
