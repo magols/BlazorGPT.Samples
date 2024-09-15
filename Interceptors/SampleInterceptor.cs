a0288798551d4e24ded3d94e2b80f6af0ececbff
Magnus Olsson <magnus72@gmail.com>
2024-09-15 12:14:18 +0200

    Sample interceptors

---
+++ Interceptors/SampleInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using BlazorGPT.Data.Model;
+using BlazorGPT.Pipeline.Interceptors;
+using Microsoft.SemanticKernel;
+
+namespace BlazorGPT.Samples.Interceptors;
+
+public class SampleContextInterceptor(IServiceProvider serviceProvider) : InterceptorBase(serviceProvider), IInterceptor
+{
+    public override string Name { get; } = "Sample Context interceptor";
+
+    public override Task<Conversation> Send(Kernel kernel, Conversation conversation,
+        Func<string, Task<string>>? onComplete = null,
+        CancellationToken cancellationToken = default)
+    {
+        if (conversation.Messages.Count == 1)
+        {
+            conversation.Messages.First().Content = "This is some added context." + conversation.Messages.First().Content;
+        }
+
+        return Task.FromResult(conversation);
+    }
+
+    public override Task<Conversation> Receive(Kernel kernel, Conversation conversation,
+        Func<string, Task<string>>? onComplete = null,
+        CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(conversation);
+    }
+}
